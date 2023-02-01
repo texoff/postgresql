d80d092bcfe759a50306ca35f63ffad792adc3d1
tex <t.test86@mail.ru>
2023-02-01 11:03:37 +0300

    add files

---
+++ chapter_3/task_4.sql
@@ -0,0 +1,6 @@
+delete from aircrafts_data where aircraft_code = 'SU7';
+--DELETE 0 данные не удалены так как условие указанное в WHERE в таблице aircrafts_data отсутствует
+
+delete from aircrafts_data where aircraft_code = 'SU8';
+--DELETE 1 удаление прошло успешно
+
